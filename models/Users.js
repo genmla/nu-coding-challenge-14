07d8a985625eccdebae0c286dd23f9bf8b2f88f9
genmla <gmmladenka@gmail.com>
2023-01-28 14:22:00 -0600

    added routes for blogposts and comments, routes for users, views for sign-up

--- models/Users.js
+++ models/Users.js
@@ -19,12 +19,20 @@ Users.init(
         name: {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING, 
+            allowNull: false,
+            unique: true, 
+            validate: {
+                isEmail:true
+            },
         }, 
         password: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [8],
+                len: [8,16],
             },
         },
     },
