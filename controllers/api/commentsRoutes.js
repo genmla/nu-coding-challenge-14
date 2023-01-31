03bdffd373b390bbf0b8da203d85ac35b7df479e
genmla <gmmladenka@gmail.com>
2023-01-30 19:32:19 -0600

    FINALLY got comments array to display comments on selected blog

--- controllers/api/commentsRoutes.js
+++ controllers/api/commentsRoutes.js
@@ -3,4 +3,18 @@ const router = require('express').Router();
 const withAuth = require('../../utils/auth');
 const path = require('path');
 
-router.post('/')
+//post a comment
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const postComment = await Comments.create({
+            ...req.body,
+            user_id: req.session.user_id
+        });
+        res.status(200).json(postComment)
+        console.log(postComment)
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
+
+module.exports = router;
